eb6d8131b11b6ba4753ed6c8299866fcb4bccd8f
Shamyralieva Zeinep <zeinepshamyralieva01@gmail.com>
2024-10-17 10:30:30 +0600

    Worked on fetchCountries function using Axios and consoling countries for the time being, added useEffect to it.

--- src/Components/CountriesApp/ContriesApp.tsx
+++ src/Components/CountriesApp/ContriesApp.tsx
@@ -1,7 +1,27 @@
 import '../../App.css';
-// const BASE_URL = 'https://restcountries.com/v2/';
-// const ALL_COUNTRIES_URL = 'all?fields=alpha3Code,name,capital,population,borders,region';
+import { ICountry } from '../../types';
+import { useState, useEffect } from 'react';
+import axios, { AxiosResponse } from 'axios';
+
+const BASE_URL = 'https://restcountries.com/v2/';
+const ALL_COUNTRIES_URL = 'all?fields=alpha3Code,name,capital,population,borders,region';
 const CountryApp= () => {
+  const [countries, setCountries] = useState<ICountry[]>([]);
+
+  const fetchCountries = async (): Promise<void> => {
+    try {
+      const response: AxiosResponse<ICountry[]> = await axios.get<ICountry[]>(`${BASE_URL}${ALL_COUNTRIES_URL}`);
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      await fetchCountries();
+    })();
+  }, []);
 
   return (
     <div className='main-container'>
@@ -9,14 +29,15 @@ const CountryApp= () => {
         <h2>Country List</h2>
       </div>
 
-      <div className='countryInfo'>
-            <img src='#' alt='flag'
-                 style={{width: '100px', height: 'auto'}}/>
-            <h2>Name</h2>
-            <p><strong>Capital:</strong> Capital</p>
-            <p><strong>Population:</strong> Population</p>
-            <p><strong>Region:</strong>Region </p>
-            <p><strong>Borders with:</strong></p>
+      <div className="countryInfo">
+        <img src="#" alt="flag"
+             style={{width: '100px', height: 'auto'}}/>
+        <h2>Name</h2>
+        <p><strong>Flag:</strong> </p>
+        <p><strong>Capital:</strong> Capital</p>
+        <p><strong>Population:</strong> Population</p>
+        <p><strong>Region:</strong>Region </p>
+        <p><strong>Borders with:</strong></p>
       </div>
     </div>
   );
---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+export interface ICountry {
+  flag:string;
+  alpha3Code: string;
+  name: string;
+  capital: string;
+  population: number;
+  borders: string[];
+  region: string;
+}
